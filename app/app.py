66f2792f2db51c009cf31d0e953a13350629e5b5
Javier Montes <yorozuya3@protonmail.com>
2022-05-08 16:17:51 +0000

    Add  load balancing

--- app/app.py
+++ app/app.py
@@ -5,14 +5,24 @@ from datetime import datetime, timedelta
 import hashlib
 from functools import wraps
 import logging
+import os
+import json
+import requests
+import statusboard as stb
 
 client = MongoClient('mongodb:27017')
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
+SECRET_KEY = os.environ['SECRET_KEY']
+EXP_TOKEN = int(os.environ['EXP_TOKEN'])
+NAME_SERVICE = os.environ['NAME_SERVICE']
+
+app.logger.info(f"SECRET KEY: {SECRET_KEY} EXPIRATION: {EXP_TOKEN}")
 
 @app.route('/signup', methods=["POST"])
 def signup():
+    stb.notify(NAME_SERVICE)
     try:
         m = hashlib.new('sha256')
         m.update(request.json['password'].encode('utf-8'))
@@ -30,10 +40,11 @@ def signup():
             return jsonify({'message':'user added'})
     except Exception as error:
         logging.info(error)
-        return 'ada'
+        return 'error'
     
 @app.route('/signin', methods=["POST"])
 def signin():
+    stb.notify(NAME_SERVICE)
     app.logger.info(request.json)
     m = hashlib.new('sha256')
     m.update(request.json['password'].encode('utf-8'))
@@ -42,22 +53,24 @@ def signin():
         "username": request.json['username'],
         "password": request.json['password'],
     }
-
+    
     with MongoClient('mongodb:27017') as client:
         db = client.users
         query = {"username":user['username'], "password": hashed_password}
-
+        
         if db.user.find_one(query):
             token = jwt.encode({
                 'public_id': user['username'],
-                'exp' : datetime.utcnow() + timedelta(minutes = 1)
-                }, "secret_key")
+                'exp' : datetime.utcnow() + timedelta(minutes = EXP_TOKEN)
+                }, SECRET_KEY)
             return make_response(jsonify({'token' : token}), 201)
         else:
             return jsonify({"message":"usuario o contraseña incorrectos"})
 
 @app.route('/auth', methods=["GET"])
 def verify_token():
+    stb.notify(NAME_SERVICE)
+
     def is_valid(exp):
         app.logger.info(datetime.utcnow().strftime('%s'))
         if (exp == 'undefined' or exp == 'null' or int(exp) > int(datetime.utcnow().strftime('%s'))):
@@ -71,7 +84,7 @@ def verify_token():
     }
     
     try:
-        decoded_token = jwt.decode(verified['token'], 'secret_key', algorithms="HS256")
+        decoded_token = jwt.decode(verified['token'], SECRET_KEY, algorithms="HS256")
         app.logger.info(decoded_token)
         if (decoded_token['public_id'] == verified['public_id']) and is_valid(decoded_token['exp']):
             return make_response(jsonify({'public_id' : decoded_token['public_id']}), 200)
